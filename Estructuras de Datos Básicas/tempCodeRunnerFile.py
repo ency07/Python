011a8687818bb48d1e7307fd3f75b3d8801554f4
ency07 <56832557+ency07@users.noreply.github.com>
2024-07-12 21:57:54 -0500

    Delete Estructuras de Datos Básicas/tempCodeRunnerFile.py

--- Estructuras de Datos Básicas/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-frase = input("\nEscibe una frase y remplacemos una letra: ")
-letra1 = input("Que letra quieres remplazar: ")
-letra2 = input("Por cual la quieres remplazar: ")
-
-resultado = frase.replace(letra1, letra2)
-print(resultado)
